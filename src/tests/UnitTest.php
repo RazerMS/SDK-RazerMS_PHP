89f4a515189f6b6c9fcf38b8b521e352196ebcc7
@jimanx2 <jiman.gman@gmail.com>
2023-04-05 09:46:50 +0800

    fix syntax error

--- src/tests/UnitTest.php
+++ src/tests/UnitTest.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-require_once(__DIR__"../support/helpers.php");
+require_once(__DIR__."/../support/helpers.php");
 
 use PHPUnit\Framework\TestCase;
 use RazerMerchantServices\Payment as RmsPayment;
